46c17259a890bf861892324b69992b0fe610567d
Akshit Madan <akmadan1717@gmail.com>
2026-01-12 15:47:35 +0530

    prometheus added in node service

--- pipeline-project-1/node-server/index.js
+++ pipeline-project-1/node-server/index.js
@@ -1,5 +1,59 @@
 const express = require('express');
+const client = require('prom-client');
+
 const app = express();
+
+// Create a Registry to register the metrics
+const register = new client.Registry();
+
+// Add a default label to all metrics
+register.setDefaultLabels({
+  app: 'node-server'
+});
+
+// Enable the collection of default metrics
+client.collectDefaultMetrics({ register });
+
+// Create custom metrics
+const httpRequestDuration = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'status_code'],
+  registers: [register]
+});
+
+const httpRequestTotal = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status_code'],
+  registers: [register]
+});
+
+// Middleware to track metrics
+app.use((req, res, next) => {
+  const start = Date.now();
+  
+  res.on('finish', () => {
+    const duration = (Date.now() - start) / 1000;
+    const route = req.route ? req.route.path : req.path;
+    
+    httpRequestDuration
+      .labels(req.method, route, res.statusCode)
+      .observe(duration);
+    
+    httpRequestTotal
+      .labels(req.method, route, res.statusCode)
+      .inc();
+  });
+  
+  next();
+});
+
+// Metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
